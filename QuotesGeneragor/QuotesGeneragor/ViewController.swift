379aee547c3915eac468b5554263bb656705a648
woo0dev <deongho9732@gmail.com>
2021-12-26 18:01:20 +0900

    project folder create

---
+++ QuotesGeneragor/QuotesGeneragor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuotesGeneragor
+//
+//  Created by Woo0 on 2021/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
