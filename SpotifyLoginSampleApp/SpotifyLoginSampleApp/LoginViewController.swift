1b6df12399aed007b365339a7a86b3dea399f72c
woo0 <woo0@woo0ui-MacBookPro.local>
2022-03-24 00:29:53 +0900

    UI Composition

--- SpotifyLoginSampleApp/SpotifyLoginSampleApp/LoginViewController.swift
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/LoginViewController.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //Navigation Bar 숨기기
+        navigationController?.navigationBar.isHidden = true
+    }
+}
