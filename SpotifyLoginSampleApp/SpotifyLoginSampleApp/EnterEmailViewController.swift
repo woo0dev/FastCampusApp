da01aa56a531fe186d49c82e079ec689f5fb5f71
woo0 <woo0@woo0ui-MacBookPro.local>
2022-03-25 23:57:37 +0900

    EnterEmailViewController create

---
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class EnterEmailViewController: UIViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = false
+    }
+}
