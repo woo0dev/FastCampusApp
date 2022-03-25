d8a4405d3e82f3626e1a17cafdc5544d5056b189
woo0 <woo0@woo0ui-MacBookPro.local>
2022-03-26 00:29:39 +0900

    EnterEmailViewController Add component

--- SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
@@ -1,9 +1,45 @@
 import UIKit
 
 class EnterEmailViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorMessageLable: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nextButton.layer.cornerRadius = 30
+        
+        nextButton.isEnabled = false
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        emailTextField.becomeFirstResponder()
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         navigationController?.navigationBar.isHidden = false
+        
+        
+    }
+    @IBAction func nextButtonTapped(_ sender: Any) {
+    }
+}
+
+extension EnterEmailViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        view.endEditing(true)
+        return false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        let isEmailEmpty = emailTextField.text == ""
+        let isPasswordEmpty = passwordTextField.text == ""
+        nextButton.isEnabled = !isEmailEmpty && !isPasswordEmpty
     }
 }
