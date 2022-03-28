dbac8103ec6308ce087cac9138855c0f6aedbaab
woo0 <woo0@woo0ui-MacBookPro.local>
2022-03-28 23:38:43 +0900

    Add login function by email

--- SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import FirebaseAuth
 
 class MainViewController: UIViewController {
     
@@ -13,6 +14,13 @@ class MainViewController: UIViewController {
         super.viewWillAppear(animated)
         
         navigationController?.navigationBar.isHidden = true
+        
+        let email = Auth.auth().currentUser?.email ?? "고객"
+        
+        welcomeLabel.text = """
+        환영합니다.
+        \(email)님
+        """
     }
     @IBAction func logoutButtonTapped(_ sender: UIButton) {
         self.navigationController?.popToRootViewController(animated: true)
