892da1ff91fd2de807bf8de887cee4974e98392e
woo0 <woo0@woo0ui-MacBookPro.local>
2022-03-30 23:51:28 +0900

    Create project and create navigation view

---
+++ MyAssets/MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by woo0 on 2022/03/30.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
