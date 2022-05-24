a69d70e2de05e88dde2db73a37f588876e062736
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-05-25 03:56:30 +0900

    github clone app

---
+++ GitHubRepository/GitHubRepository/Repository.swift
@@ -0,0 +1,21 @@
+//
+//  Repository.swift
+//  GitHubRepository
+//
+//  Created by woo0 on 2022/05/25.
+//
+
+import Foundation
+
+struct Repository: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let stargazersCount: Int
+    let language: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, description, language
+        case stargazersCount = "stargazers_count"
+    }
+}
